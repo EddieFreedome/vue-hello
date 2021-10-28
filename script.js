2f6a899749d6c84ea98edec27a6561e1e5b39f32
EddieFreedome <eddie.freed@tutanota.com>
2021-10-28 16:29:46 +0200

    Aggiunta immagine con v-bind

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const vueApp = new Vue({
     el: '#root',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        imgAstronaut: "img/astronaut_spacesuit_butterflies_121755_1920x1080.jpg"
     }
 })
