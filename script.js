61b46b35941d9801bf4cc2c9b466bfde514f46f2
EddieFreedome <eddie.freed@tutanota.com>
2021-10-28 16:45:39 +0200

    aggiunto stile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const vueApp = new Vue({
     el: '#root',
     data: {
         message: 'Hello Vue!',
-        imgAstronaut: "img/astronaut_spacesuit_butterflies_121755_1920x1080.jpg"
+        imgAstronaut: "img/astronaut_spacesuit_butterflies_121755_1920x1080.jpg",
+        classH1: "style-h1",
     }
 })
